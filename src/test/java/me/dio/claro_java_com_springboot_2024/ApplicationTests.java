bded617f8c21a3800356356763f23d33b0e579d9
Eduardoaprendizado1 <odraude447+github@gmail.com>
2024-11-15 13:26:52 -0300

    Initial commit

---
+++ src/test/java/me/dio/claro_java_com_springboot_2024/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.claro_java_com_springboot_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
